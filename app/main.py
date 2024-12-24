8f9f0b34845e131fba72aa278cc08ee4ad39bdd6
Dots109 <aleksandrnedosekin1290@gmail.com>
2024-12-24 17:50:58 +0300

    jsj

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.access.access import router as access_router
+from app.routers.users.users import router as users_router
 from app.routers.drivers.drivers import router as drivers_router
 from app.routers.mechanics.mechanics import router as mechanics_router
 from app.routers.cars.cars import router as cars_router
@@ -13,10 +14,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],  # Разрешить запросы с любого источника
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Разрешить все методы (GET, POST, OPTIONS и т.д.)
+    allow_headers=["*"],  # Разрешить все заголовки
 )
 
 
@@ -24,10 +25,11 @@ app.add_middleware(
 async def root():
     return {"message": "Let it Snow! Let it Snow! Let it Snow!"}
 
+
 app.include_router(access_router)
+app.include_router(users_router)
 app.include_router(drivers_router)
 app.include_router(mechanics_router)
 app.include_router(cars_router)
 app.include_router(fixes_router)
 app.include_router(rides_router)
-
